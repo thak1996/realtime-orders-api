6a1b486eb434beac433c2ea38584260100764f4b
Franklyn Viana dos Santos <franklyn_vs_@hotmail.com>
2025-12-02 19:38:03 -0300

    refactor: clean up DatabaseSeeder by removing commented code and ensuring password hashing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 
@@ -13,11 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'password'=> bcrypt('teste@user'),
         ]);
     }
 }
